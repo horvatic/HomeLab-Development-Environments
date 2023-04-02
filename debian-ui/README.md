9654b8fd7c99a4ca9c40218640b5d473f267b90f
horvatic <sbhorvatic@gmail.com>
2023-04-01 20:00:52 -0500

    add xfce4-terminal to UI

--- debian-ui/README.md
+++ debian-ui/README.md
@@ -3,6 +3,9 @@
 ## Overview
 Docker images that allows https connect to a UI Dev env
 
+### Example:
+Open Firefox and navigate to http://192.168.0.100:8080/
+
 ## Build
 make build
 
--- debian-ui/run.sh
+++ debian-ui/run.sh
@@ -4,4 +4,4 @@ XDG_RUNTIME_DIR=/home/homelab/xpra
 
 mkdir -p "$XDG_RUNTIME_DIR"
 
-xpra start-desktop --start=openbox --bind-tcp="0.0.0.0:8080" --daemon="no" --pulseaudio="yes" --start="tint2" --bell="no" --max-size="1280x720"
+xpra start-desktop --start=openbox --bind-tcp="0.0.0.0:8080" --daemon="no" --start="tint2" --bell="no" --max-size="1280x720"
